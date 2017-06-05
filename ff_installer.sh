b2ac52d61b7f1720593665a9f8e60fe689ba17b0
Hamed R. Nik <hamed.r.nik@gmail.com>
2017-06-05 12:32:20 +0100

    Support CentOS 7

--- ff_installer.sh
+++ ff_installer.sh
@@ -1,40 +1,53 @@
 #!/usr/bin/env bash
 
-# A Linux Shell Script for compiling and installing ffmpeg in CentOS (tested on 6).
-# This script based on ffmpeg guide <http://ffmpeg.org/trac/ffmpeg/wiki/CentosCompilationGuide>
+# A Linux Shell Script for compiling and installing ffmpeg in CentOS (tested on 7).
+# This script based on ffmpeg guide <https://trac.ffmpeg.org/wiki/CompilationGuide/Centos>
 # This script is licensed under GNU GPL version 3.0 or above.
 
 
-#Remove any existing packages:
-sudo yum -y erase ffmpeg x264 x264-devel
+# Remove any existing packages
+yum -y erase ffmpeg x264 x264-devel
+yum -y install yum-utils
 
-#Get the dependencies
-sudo yum -y install autoconf automake gcc gcc-c++ git libtool make nasm pkgconfig zlib-devel
+# Install the dependencies
+yum-config-manager --add-repo http://www.nasm.us/nasm.repo
+yum -y install autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel
 
-
+# Make a directory for FFmpeg sources
 mkdir ~/ffmpeg_sources
 
-# install Yasm
+# Install Yasm
 cd ~/ffmpeg_sources
-curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
-tar xzvf yasm-1.2.0.tar.gz
-cd yasm-1.2.0
+curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
 ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
 make
 make install
 make distclean
-. ~/.bash_profile
+source ~/.bash_profile
 
-# install x264
+# Install x264
 cd ~/ffmpeg_sources
 git clone --depth 1 git://git.videolan.org/x264
 cd x264
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
 make
 make install
 make distclean
+source ~/.bash_profile
 
-# install libfdk_aac
+# Install x265
+cd ~/ffmpeg_sources
+hg clone https://bitbucket.org/multicoreware/x265
+cd ~/ffmpeg_sources/x265/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+make install
+make distclean
+source ~/.bash_profile
+
+# Install libfdk_aac
 cd ~/ffmpeg_sources
 git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
 cd fdk-aac
@@ -43,8 +56,9 @@ autoreconf -fiv
 make
 make install
 make distclean
+source ~/.bash_profile
 
-# install libmp3lame
+# Install libmp3lame
 cd ~/ffmpeg_sources
 curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xzvf lame-3.99.5.tar.gz
@@ -53,65 +67,75 @@ cd lame-3.99.5
 make
 make install
 make distclean
+source ~/.bash_profile
 
-# install libopus
+# Install libopus
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
-tar xzvf opus-1.0.3.tar.gz
-cd opus-1.0.3
+curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
+tar xzvf opus-1.1.5.tar.gz
+cd opus-1.1.5
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
 make install
 make distclean
+source ~/.bash_profile
 
-# install libogg
+# Install libogg
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
-tar xzvf libogg-1.3.1.tar.gz
-cd libogg-1.3.1
+curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+tar xzvf libogg-1.3.2.tar.gz
+cd libogg-1.3.2
 ./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
 make install
 make distclean
+source ~/.bash_profile
+#yum -y install libogg
 
-# install libvorbis
+# Install libvorbis
 cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
-tar xzvf libvorbis-1.3.3.tar.gz
-cd libvorbis-1.3.3
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+tar xzvf libvorbis-1.3.4.tar.gz
+cd libvorbis-1.3.4
 ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
 make
 make install
 make distclean
+source ~/.bash_profile
+#yum -y install libvorbis
 
-# install libtheora
-cd ~/src/ffmpeg-source
-wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
-tar xzvf libtheora-1.1.1.tar.gz
-cd libtheora-1.1.1
-./configure --disable-shared
-make
-sudo make install
+# Install libtheora
+# cd ~/ffmpeg_sources
+# wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
+# tar xzvf libtheora-1.1.1.tar.gz
+# cd libtheora-1.1.1
+# ./configure --disable-shared
+# make
+# make install
+# make distclean
+# source ~/.bash_profile
+yum -y install libtheora
 
-
-# install libvpx
+# Install libvpx
 cd ~/ffmpeg_sources
 git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
 cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples
-make
-make install
-make clean
-
-# install FFmpeg
-cd ~/ffmpeg_sources
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg
-cd ffmpeg
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
-export PKG_CONFIG_PATH
-./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264
-make
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples  --as=yasm
+PATH="$HOME/bin:$PATH" make
 make install
 make distclean
+source ~/.bash_profile
+#yum -y install libvpx
+
+# Install FFmpeg
+cd ~/ffmpeg_sources
+curl -O http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+tar xjvf ffmpeg-snapshot.tar.bz2
+cd ffmpeg
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib -ldl" --bindir="$HOME/bin" --pkg-config-flags="--static" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libfreetype --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265
+make
+make install
 hash -r
-. ~/.bash_profile
+
+make distclean
+source ~/.bash_profile
